0ae787bdfcf8f4144dadb2a674922d6d06636391
yandirad <yandirad@gmail.com>
2021-12-23 17:12:24 -0300

    fix bug Class/ClassName

--- src/components/public/Footer.js
+++ src/components/public/Footer.js
@@ -2,18 +2,18 @@ import React from 'react'
 
 export const Footer = () => {
     return (
-        <div class="container">
-            <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-                <div class="col-md-4 d-flex align-items-center">
+        <div className="container">
+            <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+                <div className="col-md-4 d-flex align-items-center">
 
-                    <span class="text-muted">&copy; 2021 Company, Inc</span>
+                    <span className="text-muted">&copy; 2021 Company, Inc</span>
                 </div>
-                <ul class="nav col-md-4 justify-content-end">
-                    <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+                <ul className="nav col-md-4 justify-content-end">
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Home</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Features</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Pricing</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">FAQs</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">About</a></li>
                 </ul>
             </footer>
         </div>
