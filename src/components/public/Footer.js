777478d38976d0d0230d4e3ac73558858cdf74c6
yandirad <yandirad@gmail.com>
2022-01-25 11:22:44 +0100

    update footer

--- src/components/public/Footer.js
+++ src/components/public/Footer.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Footer = () => {
+
+    const [clicks, setClicks] = useState(0);
+
+    const year = new Date().getFullYear();
+    const owner = "yandirad";
+
+    const handleClick = () => {
+        setClicks(clicks + 1);
+    }
+
     return (
         <div className="container">
-            <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-                <div className="col-md-4 d-flex align-items-center">
+            <footer className="d-flex flex-wrap justify-content-between border-top">
+                <p className="text-muted">&copy; {year} {owner} Perrito apretado {clicks} veces</p>
+                <span 
+                    className="col-md-3 d-flex align-items-center justify-content-center link-dark text-decoration-none"
+                    onClick={handleClick}
+                    >
+                    
+                    <img
+                        className="App-logo"
+                        height="52"
+                        src="mascotapp/dog.png"
+                        alt="perro-giratorio"
+                    />
+                </span>
 
-                    <span className="text-muted">&copy; 2021 Company, Inc</span>
-                </div>
-                <ul className="nav col-md-4 justify-content-end">
+                <ul className="nav col-md-3 justify-content-end">
                     <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Home</a></li>
                     <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Features</a></li>
                     <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Pricing</a></li>
